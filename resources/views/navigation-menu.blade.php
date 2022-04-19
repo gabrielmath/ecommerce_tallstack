4bcec846db5b84784a611cdf98e2dc0bd7ace6dd
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2022-04-19 20:10:13 -0300

    Initial navigation menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -14,7 +14,13 @@
         <!-- Navigation Links -->
         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
           <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
+            Products
+          </x-jet-nav-link>
+          <x-jet-nav-link href="#">
+            About
+          </x-jet-nav-link>
+          <x-jet-nav-link href="#">
+            Contact
           </x-jet-nav-link>
         </div>
       </div>
