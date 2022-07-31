68d8706b32dd7ca5c910c5977cac1e3d69958814
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2022-07-31 01:15:42 -0300

    feat(admin): change product list actions layout

--- resources/views/livewire/admin/product-list.blade.php
+++ resources/views/livewire/admin/product-list.blade.php
@@ -1,27 +1,31 @@
 <div>
-  <div class="flex items-end">
-    <h1 class="font-bold text-3xl text-gray-700">Products ({{ $this->products->total() }})</h1>
+  <div class="flex items-center justify-between">
+    <div>
+      <h1 class="font-bold text-3xl text-gray-700">Products ({{ $this->products->total() }})</h1>
 
-    <x-input.search
-      placeholder="Search..."
-      wire:model.debounce.500ms="search"
-      :is-active="!!$search"
-      class="max-w-xs ml-7"
-    />
+      <div class=" flex items-center space-x-4">
+        <div class="ml-auto flex items-center space-x-6">
+          <x-filter-item wire:click="$set('filter', null)" :is-active="!$filter">
+            All
+          </x-filter-item>
 
-    <div class="ml-auto flex items-center space-x-6">
-      <x-filter-item wire:click="$set('filter', null)" :is-active="!$filter">
-        All
-      </x-filter-item>
+          <x-filter-item wire:click="$set('filter','draft')" :is-active="$filter === 'draft'">
+            Draft
+          </x-filter-item>
 
-      <x-filter-item wire:click="$set('filter','draft')" :is-active="$filter === 'draft'">
-        Draft
-      </x-filter-item>
-
-      <x-filter-item wire:click="$set('filter','published')" :is-active="$filter === 'published'">
-        Published
-      </x-filter-item>
+          <x-filter-item wire:click="$set('filter','published')" :is-active="$filter === 'published'">
+            Published
+          </x-filter-item>
+        </div>
+        <x-input.search
+          placeholder="Search..."
+          wire:model.debounce.500ms="search"
+          :is-active="!!$search"
+          class="max-w-xs ml-7"
+        />
+      </div>
     </div>
+    <x-jet-button>New Product</x-jet-button>
   </div>
 
   <div class="grid grid-cols-3 gap-5 mt-6">
