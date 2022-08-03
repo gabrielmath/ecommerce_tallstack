b818e792871820003007de0e0ffb366904f5602a
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2022-08-03 00:32:28 -0300

    feat(admin): add Sortablejs package

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,11 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import SimpleMaskMoney from 'simple-mask-money';
 import currency from 'currency.js';
+import Sortablejs from 'sortablejs';
 
 window.Alpine = Alpine;
 window.currency = currency;
 window.SimpleMaskMoney = SimpleMaskMoney;
+window.Sortablejs = Sortablejs;
 
 Alpine.start();
