6ccd15c049818f21a4b3a5b39c3afbb7b7fe6527
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2023-02-03 21:14:42 -0300

    feat(admin-product): PublishTest Backup

---
+++ tests/Feature/Http/Livewire/Admin/ProductForm/PublishTest.php
@@ -0,0 +1,7 @@
+publish<?php
+
+it('has http/livewire/admin/productform/publish page', function () {
+    $response = $this->get('/http/livewire/admin/productform/publish');
+
+    $response->assertStatus(200);
+});
